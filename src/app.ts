16c7f6fc83dce7b38ed481ba942e57f3054fd467
Bashaier AlMeshaileh <56195895+bkmushaileh@users.noreply.github.com>
2025-08-28 23:21:55 +0300

    Done

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import express from "express";
 import connectDB from "./database";
 import postsRoutes from "./api/posts/posts.routes";
-
-
+import { notFoundHandler } from "./middlewares/notFound.middleware";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 const app = express();
 const PORT = 8000;
+connectDB();
 
 app.use(express.json());
-
+app.use(morgan("dev"));
+app.use(cors());
+app.use("/media", express.static(path.join(__dirname, "../media")));
 app.use("/posts", postsRoutes);
+app.use(errorHandler);
+app.use(notFoundHandler);
 
-
-connectDB();
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- src/database.ts
+++ src/database.ts
@@ -6,13 +6,14 @@ dotenv.config();
 console.log(process.env.MONGODB_URI);
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || "");
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error("MongoDB connection failed:", error);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || "");
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(process.env.MONGODB_URI);
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
